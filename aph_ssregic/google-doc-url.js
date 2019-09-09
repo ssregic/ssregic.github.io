e5b7b2e9ce448281db8fad82d8b10a29670a7e3c
SSREGIC <pdigcba@gmail.com>
2019-09-09 19:20:44 -0300

    Update google-doc-url.js

--- aph_ssregic/google-doc-url.js
+++ aph_ssregic/google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lGHkjap-aLHBFNx4Ay0tazegvuhY2iradVtbfgpJ990/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhVXauKdjp9RGd5CsD26Uho26_oTjElT8AZ0_W8chv5Qcw07pi0Nd8Ou4pzDJhmrNWzzW6kFKcThyB/pubhtml';
